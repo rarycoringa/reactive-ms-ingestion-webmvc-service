99c93a73ffb5b6c53895d341f181b18d717fd307
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-09-22 18:01:08 -0300

    enable webmvc on main app class

--- src/main/java/br/edu/ufrn/ingestion/IngestionApplication.java
+++ src/main/java/br/edu/ufrn/ingestion/IngestionApplication.java
@@ -2,8 +2,10 @@ package br.edu.ufrn.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class IngestionApplication {
 
 	public static void main(String[] args) {
